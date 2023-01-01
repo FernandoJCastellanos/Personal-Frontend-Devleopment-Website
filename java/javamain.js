2bff80c14a0a1558a05523e2908f0faf2023a607
FernandoJC <fernandojcastellanos@gmail.com>
2023-01-01 11:36:01 -0400

    Inital commit for Frontend Portatolio Site

---
+++ java/javamain.js
@@ -0,0 +1,249 @@
+
+// HAPPY FACE LOTTIE ANIMATION
+const animation = bodymovin.loadAnimation({
+    container: document.getElementById('happy-face'),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: 'data.json'
+})
+
+
+
+
+
+
+// spaceTourism Java
+// spaceTourism Java
+
+// java image
+document.querySelector("#youtubeSpaceTourismJava").onclick = function() {
+    // java video
+        const youtubeSpaceTourism = document.querySelector("#youtubeSpaceTourismPlayerJava");
+    
+        youtubeSpaceTourism.style.display = "block";
+    
+        };
+    
+    
+    // Elements Selected
+    
+    // box to look
+    var spaceTourismBox = document.querySelector(".spaceTourismBox");
+    // text to attach
+    var spaceTourismText = document.querySelector(".spaceTourismText");
+    
+    
+    // Resize observer Set
+    // rename observer function
+    const spaceTourismTextObserver = new ResizeObserver(entries =>{
+    const element = entries[0];
+    const elementHeightLog = element.contentRect.height;
+    // change text to attach
+    spaceTourismText.style.height = elementHeightLog + "px";
+    });
+    
+    // change box to look
+    spaceTourismTextObserver.observe(spaceTourismBox);
+
+
+
+
+
+
+
+
+
+// baseApparel Java
+// baseApparel Java
+
+// java image
+document.querySelector("#youtubeBaseApparelJava").onclick = function() {
+    // java video
+        const youtubeBaseApparel = document.querySelector("#youtubeBaseApparelPlayerJava");
+    
+        youtubeBaseApparel.style.display = "block";
+    
+        };
+    
+    
+    // Elements Selected
+    
+    // box to look
+    var baseApparelBox = document.querySelector(".baseApparelBox");
+    // text to attach
+    var baseApparelText = document.querySelector(".baseApparelText");
+    
+    
+    // Resize observer Set
+    // rename observer function
+    const baseApparelTextTextObserver = new ResizeObserver(entries =>{
+    const element = entries[0];
+    const elementHeightLog = element.contentRect.height;
+    // change text to attach
+    baseApparelText.style.height = elementHeightLog + "px";
+    });
+    
+    // change box to look
+    baseApparelTextTextObserver.observe(baseApparelBox);
+
+
+
+
+
+
+
+// sunnyside Java
+// sunnyside Java
+
+// java image
+document.querySelector("#youtubesunnysideJava").onclick = function() {
+    // java video
+        const youtubesunnyside = document.querySelector("#youtubesunnysidePlayerJava");
+    
+        youtubesunnyside.style.display = "block";
+    
+        };
+    
+    
+    // Elements Selected
+    
+    // box to look
+    var sunnysideBox = document.querySelector(".sunnysideBox");
+    // text to attach
+    var sunnysideText = document.querySelector(".sunnysideText");
+    
+    
+    // Resize observer Set
+    // rename observer function
+    const sunnysideTextObserver = new ResizeObserver(entries =>{
+    const element = entries[0];
+    const elementHeightLog = element.contentRect.height;
+    // change text to attach
+    sunnysideText.style.height = elementHeightLog + "px";
+    });
+    
+    // change box to look
+    sunnysideTextObserver.observe(sunnysideBox);
+
+
+
+
+
+
+
+
+// ArtPlayer Java
+// ArtPlayer Java
+
+// java image
+document.querySelector("#youtubeArtJava").onclick = function() {
+    // java video
+        const youtubeArt = document.querySelector("#youtubeArtPlayerJava");
+    
+        youtubeArt.style.display = "block";
+    
+        };
+    
+    
+    // Elements Selected
+    
+    // box to look
+    var artBox = document.querySelector(".artBox");
+    // text to attach
+    var artText = document.querySelector(".artText");
+    
+    
+    // Resize observer Set
+    const artTextObserver = new ResizeObserver(entries =>{
+    const element = entries[0];
+    const elementHeightLog = element.contentRect.height;
+    // change text to attach
+    artText.style.height = elementHeightLog + "px";
+    });
+    
+    // change box to look
+    artTextObserver.observe(artBox);
+
+
+
+
+
+// DrumKitPlayer Java
+// DrumKitPlayer Java
+
+// java image
+document.querySelector("#youtubeDrumKitJava").onclick = function() {
+    // java video
+        const youtubeDrumKit = document.querySelector("#youtubeDrumKitplayerJava");
+    
+        youtubeDrumKit.style.display = "block";
+    
+        };
+    
+    
+    // Elements Selected
+    
+    // box to look
+    var drumkitBox = document.querySelector(".drumkitBox");
+    // text to attach
+    var drumkitText = document.querySelector(".drumkitText");
+    
+    
+    // Resize observer Set
+    const drumkitTextObserver = new ResizeObserver(entries =>{
+    const element = entries[0];
+    const elementHeightLog = element.contentRect.height;
+    // change text to attach
+    drumkitText.style.height = elementHeightLog + "px";
+    });
+    
+    // change box to look
+    drumkitTextObserver.observe(drumkitBox);
+
+
+
+
+
+
+
+
+// TinDogPlayer Java
+// TinDogPlayer Java
+
+// java image
+document.querySelector("#youtubeTinDogJava").onclick = function() {
+// java video
+    const youtubeTinDog = document.querySelector("#youtubeTinDogPlayerJava");
+
+    youtubeTinDog.style.display = "block";
+
+    };
+
+
+// Elements Selected
+
+// box to look
+var tinDogBox = document.querySelector(".tindogBox");
+// text to attach
+var tinDogText = document.querySelector(".tinDogText");
+
+
+// Resize observer Set
+const tindogTextObserver = new ResizeObserver(entries =>{
+const element = entries[0];
+const elementHeightLog = element.contentRect.height;
+// change text to attach
+tinDogText.style.height = elementHeightLog + "px";
+});
+
+// change box to look
+tindogTextObserver.observe(tinDogBox);
+
+
+
+
+
+
+
+
